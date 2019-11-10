3cdbdc4effc5ea011be08b849ac4ef7d841d10f1
Dromaf <pdvxai@gmail.com>
2019-11-10 15:22:57 +0200

    edit filter.jsx

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -6,41 +6,6 @@ import Search from "../search/search";
 import { Link } from 'react-router-dom';
 import Filter from "../filter/filter";
 
-const translation = {
-  Shevchenkovskiy: "Шевченковский",
-  Oktyabrksiy: "Подольский",
-  Kievskiy: "Киевский",
-  pub: "Паб",
-  restaurant: " Ресторан",
-  cafe: " Кафе",
-  teahouse: " Чайная",
-  coffeehouse: " Кофейня",
-  bar: " Бар",
-  hookah: " Кальянная",
-  steakhouse: " Cтейк-хаус",
-  ukrainian: " Украинская",
-  europinian: " Европейская",
-  сaucasian: " Кавказская",
-  itlian: " Итальянская",
-  grill: " Гриль",
-  seafood: " Морепродукты",
-  japaneese: " Японская",
-  desserts: " Дессерты",
-  asian: " Азиатская",
-  chineese: " Китайская",
-  mexican: " Мексиканская",
-  american: " Американская",
-  kidsRoom: " Детская комната ", //Детские комнаты
-  reservation: " Бронь столов",  // Бронь столов
-  DressingRoom: " Гардеробная",//гардероб
-  cardPayment: " Оплата картой", //Оплата картой
-  parking: " Парковка", //Парковка
-  delivery: " Доставка", //доставка
-  wifi: " Wi-Fi",
-  takeaway: " Вынос еды с собой", //еда или напитки на вынос
-  plaseForSmoking: " Место для курения",
-  iqosFriendly: " Iqos friendly",
-}
 class Content extends React.Component {
   constructor(props) {
     super(props)
@@ -56,6 +21,9 @@ class Content extends React.Component {
 
   onSearchChange(term) {
     this.setState({ term });
+  };
+  onOptionChange(options) {
+    this.setState({ options });
   };
 
   search(items, term) {
@@ -86,100 +54,14 @@ class Content extends React.Component {
     return filterRests;
   }
 
-  renderCheckboxesDistrict() {
-    const status = this.props.state.restaurDb[0].district
-    return Object.keys(status).map((district, index) => {
-      let childtypeFilter = [];
-      for (let key in status) {
-        childtypeFilter.push(`${translation[key]}`);
-      }
-      return (
-        <div className={s.checkboxHeight} key={index}>
-          <label className="checkbox" >
-            <input
-              onChange={this.onChange.bind(this)}
-              type="checkbox"
-              name={district}
-            />
-            <div className="checkbox__text"> {childtypeFilter[index]}</div>
-          </label>
-        </div>
-      );
-    });
-  }
-  renderCheckboxesType() {
-    const status = this.props.state.restaurDb[0].type
-    return Object.keys(status).map((type, index) => {
-      let childtypeFilter = [];
-      for (let key in status) {
-        childtypeFilter.push(`${translation[key]}`);
-      }
-      return (
-        <div className={s.checkboxHeight} key={index}>
-          <label className="checkbox" >
-            <input
-              onChange={this.onChange.bind(this)}
-              type="checkbox"
-              name={type}
-            />
-            <div className="checkbox__text"> {childtypeFilter[index]}</div>
-          </label>
-        </div>
-      );
-    });
-  }
-
-  renderCheckboxesСuisine() {
-    const status = this.props.state.restaurDb[0].cuisine
-    return Object.keys(status).map((cuisine, index) => {
-      let childtypeFilter = [];
-      for (let key in status) {
-        childtypeFilter.push(`${translation[key]}`);
-      }
-      return (
-        <div className={s.checkboxHeight} key={index}>
-          <label className="checkbox" >
-            <input
-              onChange={this.onChange.bind(this)}
-              type="checkbox"
-              name={cuisine}
-            />
-            <div className="checkbox__text"> {childtypeFilter[index]}</div>
-          </label>
-        </div>
-      );
-    });
-  }
-
-  onChange(e) {
-    const options = this.state.options
-    let index
-    if (e.target.checked) {
-      options.push(e.target.name)
-    } else {
-      index = options.indexOf(e.target.name)
-      options.splice(index, 1)
-    }
-    this.setState({ options: options })
-  }
-
   render() {
     const visibleItems = this.search(this.getItems(), this.state.term);
     const numberOfItems = this.state.showMore ? visibleItems.length : 5;
+
     return (
       <div className={s.bgfon} >
         <Header />
-        <Filter />
-        <div className={s.checkboxName}>Районы
-          <div className={s.checkboxSize_distr}> {this.renderCheckboxesDistrict()}</div>
-        </div>
-        <div className={s.checkboxName}>Типы заведений
-          <div className={s.checkboxSize}>{this.renderCheckboxesType()}</div>
-        </div>
-        <div className={s.checkboxName}>Кухня
-          <div className={s.checkboxSize}> {this.renderCheckboxesСuisine()}</div>
-        </div>
-
+        <Filter {...this.props} onOptionChange={this.onOptionChange.bind(this)} />
         <Search onSearchChange={this.onSearchChange.bind(this)} />
         {visibleItems.slice(0, numberOfItems).map((item) => {
           return (
