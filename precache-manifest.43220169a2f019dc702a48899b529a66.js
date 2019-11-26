772221b7e1e0d3072cff6d0812ef18ec5b2b72ff
dromaf <pdvxai@gmail.com>
2019-11-26 14:02:48 +0200

    Updates

---
+++ precache-manifest.43220169a2f019dc702a48899b529a66.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506f02469f9a889daa5eaa28e2f888f1",
+    "url": "/Dromaf/restDB/index.html"
+  },
+  {
+    "revision": "c2d47f1f974c9c81a091",
+    "url": "/Dromaf/restDB/static/css/2.72c2318d.chunk.css"
+  },
+  {
+    "revision": "5cb2b5ac0ab72552ce1b",
+    "url": "/Dromaf/restDB/static/css/main.030453cc.chunk.css"
+  },
+  {
+    "revision": "c2d47f1f974c9c81a091",
+    "url": "/Dromaf/restDB/static/js/2.a8e12b76.chunk.js"
+  },
+  {
+    "revision": "5cb2b5ac0ab72552ce1b",
+    "url": "/Dromaf/restDB/static/js/main.bddd9e31.chunk.js"
+  },
+  {
+    "revision": "128ab4706c0c5a410dea",
+    "url": "/Dromaf/restDB/static/js/runtime-main.4eca0bb9.js"
+  },
+  {
+    "revision": "e9ebf5183a702ba6498d60b5092737bc",
+    "url": "/Dromaf/restDB/static/media/appetite.e9ebf518.woff"
+  },
+  {
+    "revision": "30893e0d9b37eeeaf196a22b4fef28cb",
+    "url": "/Dromaf/restDB/static/media/back.30893e0d.svg"
+  },
+  {
+    "revision": "dfbed0619ad891bab09782ed5006f6d9",
+    "url": "/Dromaf/restDB/static/media/bg-fon.dfbed061.jpg"
+  },
+  {
+    "revision": "94c9d2040f7c8704e26cc2ce8dfcd9e9",
+    "url": "/Dromaf/restDB/static/media/bgloc.94c9d204.png"
+  },
+  {
+    "revision": "2b8fb109a17089e04d538d528759a708",
+    "url": "/Dromaf/restDB/static/media/filter.2b8fb109.svg"
+  },
+  {
+    "revision": "7157e475561682e5bcbd570eb4d35817",
+    "url": "/Dromaf/restDB/static/media/star.7157e475.svg"
+  },
+  {
+    "revision": "313b329114d02fe30148c72560ee3595",
+    "url": "/Dromaf/restDB/static/media/trash.313b3291.svg"
+  }
+]);
